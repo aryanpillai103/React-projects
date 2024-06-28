a08c2ca5e49ac9bed8aedfe3165bff385fe3eaed
Aryan Pillai <aryanpillai518@gmail.com>
2024-06-29 01:34:56 +0530

    Created counter project to learn hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  let [counter, setCounter] = useState(15)
+
+  //let counter = 15
+  const addValue = () => {
+    if(counter < 20){
+      setCounter(counter+1)
+    }
+    
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter-1);
+    }
+    else{
+      removeValue.disabled = true;
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button 
+      onClick={addValue}
+      >Add value: {counter}</button>
+      <br/>
+      
+      <button 
+      onClick={removeValue}>
+        Remove Value: {counter}</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
